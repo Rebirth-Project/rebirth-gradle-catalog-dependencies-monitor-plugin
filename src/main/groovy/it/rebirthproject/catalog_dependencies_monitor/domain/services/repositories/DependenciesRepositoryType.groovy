5915531b0413cd5cead216a811150eadd20650be
patton <andrea.paternesi@gmail.com>
2025-10-22 17:56:50 +0200

    Fix for typo

--- src/main/groovy/it/rebirthproject/catalog_dependencies_monitor/domain/services/repositories/DependenciesRepositoryType.groovy
+++ src/main/groovy/it/rebirthproject/catalog_dependencies_monitor/domain/services/repositories/DependenciesRepositoryType.groovy
@@ -18,7 +18,7 @@ package it.rebirthproject.catalog_dependencies_monitor.domain.services.repositor
 
 enum DependenciesRepositoryType {
     MAVEN_CENTRAL_V1("Maven Central", "Maven Central", "https://mvnrepository.com","https://search.maven.org"),
-    MAVEN_CENTRAL_v2("Maven Central Sonatype", "Maven Central Sonatype", "https://central.sonatype.com/","https://repo.maven.apache.org"),
+    MAVEN_CENTRAL_V2("Maven Central Sonatype", "Maven Central Sonatype", "https://central.sonatype.com/","https://repo.maven.apache.org"),
     GRADLE_PLUGINS_PORTAL("Gradle", "Gradle Plugins Portal", "https://plugins.gradle.org","")
 
     final String title
